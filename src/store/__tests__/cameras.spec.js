9209fb9be1708a1e24e24ad7894c9bfc962a474b
Kevin Killingsworth <kevin@redfenix.com>
2019-09-10 11:15:40 -0500

    ✅ Adding jest configuration and initial test
    This adds a small initial test and jest configuration to run it in node.

---
+++ src/store/__tests__/cameras.spec.js
@@ -0,0 +1,12 @@
+import cameras from '../cameras.js';
+
+describe( 'store state: cameras', () => {
+	describe( 'reducer', () => {
+		const { reducer } = cameras;
+
+		it( 'should return an empty default state', () => {
+			const state = reducer();
+			expect( state ).toEqual( {} );
+		} );
+	} );
+} );
